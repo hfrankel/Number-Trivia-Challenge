6c3ee88dc1104804e20cc52a3e1b846f81d5f604
Harry Frankel <hfranks92@gmail.com>
2020-06-29 16:12:50 +1000

    Installed eslint-react-hooks-plugin and refactored to use hooks instead

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 import Number from './Number';
 
-class App extends React.Component {
-  state = { number: '' };
+const App = () => {
+  const [number, setNumber] = useState('');
 
-  onSearchSubmit = async (numberSel, option) => {
+  const onSearchSubmit = async (numberSel, option) => {
     const response = await axios.get(
       `http://numbersapi.com/${numberSel}/${option}`
     );
 
-    this.setState({ number: response.data });
+    setNumber(response.data);
   };
 
-  render() {
-    return (
-      <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        <Number number={this.state.number} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="ui container" style={{ marginTop: '10px' }}>
+      <SearchBar onSubmit={onSearchSubmit} />
+      <Number number={number} />
+    </div>
+  );
+};
 
 export default App;
